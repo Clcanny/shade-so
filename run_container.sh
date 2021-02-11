58353b0a020bc7237e0d80c3dc5a48367816efc6
Clcanny <a837940593@gmail.com>
2021-02-12 00:46:46 +0800

    .

--- run_container.sh
+++ run_container.sh
@@ -5,5 +5,5 @@ if $(docker ps | grep -q "shade-so"); then
     docker start -a -i shade-so
 else
     echo "Container doesn't exist, start creating..."
-    docker run -it -v /home/demons/LIEF:/LIEF -v /home/demons/zydis:/zydis -v $PWD:/root/shade-so shade-so:0.1 bash
+    docker run -it -v $PWD:/root/shade-so --privileged --name shade-so shade-so:0.1 bash
 fi
