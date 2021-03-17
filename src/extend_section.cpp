d45bf3bed0651442fc47aba7e9624aadd2fbfcb8
Clcanny <a837940593@gmail.com>
2021-03-17 15:16:46 +0800

    add sec malloc manager

--- src/extend_section.cpp
+++ src/extend_section.cpp
@@ -9,8 +9,8 @@
 
 #include <array>
 #include <cassert>
-
-#include "src/patch_rip_insts.h"
+#include <cmath>
+#include <numeric>
 
 namespace shade_so {
 
@@ -37,6 +37,65 @@ void ExtendSection::ceil_size() {
     if (size_ % align != 0) {
         size_ = (size_ / align + 1) * align;
     }
+}
+
+SecMallocMgr::SecMallocMgr(const LIEF::ELF::Binary& artifact,
+                           const LIEF::ELF::Binary& dependency,
+                           LIEF::ELF::Binary* fat,
+                           const std::string& name,
+                           int max_alloc_times)
+    : artifact_(artifact), dependency_(dependency), fat_(fat), name_(name),
+      max_alloc_times_(max_alloc_times), size_(0), capacity_(0) {
+    assert(fat_ != nullptr);
+    if (fat->has_section(name_)) {
+        sec_ = &fat_->get_section(name_);
+    } else {
+    }
+    assert(sec_ != nullptr);
+
+    sec_align_ = 1;
+    elf_align_ = 1;
+    for (const auto& bin : std::array<const LIEF::ELF::Binary*, 3>{
+             &artifact_, &dependency_, fat_}) {
+        sec_align_ = std::lcm(
+            sec_align_,
+            bin->has_section(name_) ? bin->get_section(name_).alignment() : 1);
+        for (const auto& sec : bin->sections()) {
+            elf_align_ = std::lcm(elf_align_, sec.alignment());
+        }
+    }
+
+    size_ = sec_->size();
+    capacity_ = size_;
+}
+
+int64_t SecMallocMgr::malloc(int64_t size) {
+    assert(blocks_.size() < max_alloc_times_);
+    size = std::ceil(size * 1.0 / sec_align_) * sec_align_;
+    assert(size_ <= capacity_);
+    size_ = std::ceil(size_ * 1.0 / sec_align_) * sec_align_;
+    if (size_ + size > capacity_) {
+        auto extend_size =
+            std::ceil((size_ + size) * 1.0 / elf_align_) * elf_align_ -
+            capacity_;
+        fat_->extend(*sec_, extend_size);
+        capacity_ = size_ + extend_size;
+        assert(capacity_ == sec_->size());
+        assert(size_ + size <= capacity_);
+    }
+    auto start = size_;
+    size_ += size;
+    assert(blocks_.emplace(start, size).second);
+    return start;
+}
+
+int64_t SecMallocMgr::malloc_artifact() {
+    return malloc(artifact_.get_section(name_).size());
+}
+
+int64_t SecMallocMgr::latest_block_sa() const {
+    assert(blocks_.rbegin() != blocks_.rend());
+    return blocks_.rbegin()->first;
 }
 
 }  // namespace shade_so
--- src/extend_section.h
+++ src/extend_section.h
@@ -8,6 +8,8 @@
 #ifndef SRC_EXTEND_SECTION_H_
 #define SRC_EXTEND_SECTION_H_
 
+#include <cstdint>
+#include <map>
 #include <string>
 
 #include <LIEF/ELF.hpp>
@@ -29,6 +31,35 @@ class ExtendSection {
     Binary* bin_;
     const Section& section_;
     uint64_t size_;
+};
+
+class SecMallocMgr {
+ public:
+    SecMallocMgr(const LIEF::ELF::Binary& artifact,
+                 const LIEF::ELF::Binary& dependency,
+                 LIEF::ELF::Binary* fat,
+                 const std::string& name,
+                 int max_alloc_times = 1);
+    int64_t malloc(int64_t size);
+    int64_t malloc_artifact();
+    int64_t latest_block_sa() const;
+
+ private:
+    const LIEF::ELF::Binary& artifact_;
+    const LIEF::ELF::Binary& dependency_;
+
+    LIEF::ELF::Binary* const fat_;
+    LIEF::ELF::Section* sec_;
+    std::string name_;
+    int64_t sec_align_;
+    int64_t elf_align_;
+
+    // Start address to size.
+    std::map<int64_t, int64_t> blocks_;
+    int max_alloc_times_;
+
+    int64_t size_;
+    int64_t capacity_;
 };
 
 }  // namespace shade_so
