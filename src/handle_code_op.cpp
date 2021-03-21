d63e7ea70a2fb39f507679f9887d8761f8282e05
Clcanny <a837940593@gmail.com>
2021-03-21 23:57:36 +0800

    .

--- src/handle_code_op.cpp
+++ src/handle_code_op.cpp
@@ -11,60 +11,58 @@
 #include <set>
 #include <string>
 
+#include "src/const.h"
+
 namespace shade_so {
 
 HandleCodeOp::HandleCodeOp(OperatorArgs args) : args_(args) {
 }
 
 void HandleCodeOp::extend() {
-    uint8_t nop_code = 0x90;
-    text_off_ =
-        args_.sec_malloc_mgr_->get_or_create(".text").malloc_dependency();
+    text_off_ = args_.sec_malloc_mgr_->get_or_create(sec_names::kText)
+                    .malloc_dependency();
 }
 
 void HandleCodeOp::merge() {
-    auto src_ = const_cast<LIEF::ELF::Binary*>(&args_.dependency_);
-    auto dst_ = const_cast<LIEF::ELF::Binary*>(&args_.artifact_);
-    auto out_ = args_.fat_;
-
-    merge_section(*src_, out_, ".text", text_off_);
-    const LIEF::ELF::Section& src_text_sec = src_->text_section();
-    const LIEF::ELF::Section& dst_text_sec = dst_->text_section();
-    const LIEF::ELF::Section& out_text_sec = out_->text_section();
-    auto out_text_id =
-        std::find_if(out_->sections().begin(),
-                     out_->sections().end(),
-                     [&out_text_sec](const LIEF::ELF::Section& sec) {
-                         return sec == out_text_sec;
+    merge_section(args_.dependency_, args_.fat_, sec_names::kText, text_off_);
+    const LIEF::ELF::Section& dep_text_sec =
+        args_.dependency_.get_section(sec_names::kText);
+    const LIEF::ELF::Section& fat_text_sec =
+        args_.fat_->get_section(sec_names::kText);
+    auto fat_text_id =
+        std::find_if(args_.fat_->sections().begin(),
+                     args_.fat_->sections().end(),
+                     [&fat_text_sec](const LIEF::ELF::Section& sec) {
+                         return sec == fat_text_sec;
                      }) -
-        out_->sections().begin();
+        args_.fat_->sections().begin();
     std::set<std::string> addedSyms;
-    for (auto i = 0; i < src_->symbols().size(); i++) {
-        const LIEF::ELF::Symbol& src_sym = src_->symbols()[i];
-        if (!src_sym.is_function() || src_sym.is_imported()) {
+    for (auto i = 0; i < args_.dependency_.symbols().size(); i++) {
+        const LIEF::ELF::Symbol& dep_sym = args_.dependency_.symbols()[i];
+        if (!dep_sym.is_function() || dep_sym.is_imported()) {
             continue;
         }
-        if (!(src_sym.value() >= src_text_sec.virtual_address() &&
-              src_sym.value() <
-                  src_text_sec.virtual_address() + src_text_sec.size())) {
+        if (!(dep_sym.value() >= dep_text_sec.virtual_address() &&
+              dep_sym.value() <
+                  dep_text_sec.virtual_address() + dep_text_sec.size())) {
             continue;
         }
-        const std::string& name = src_sym.name();
+        const std::string& name = dep_sym.name();
         if (addedSyms.find(name) != addedSyms.end()) {
             continue;
         }
         addedSyms.insert(name);
-        LIEF::ELF::Symbol out_sym(
+        LIEF::ELF::Symbol fat_sym(
             name,
             LIEF::ELF::ELF_SYMBOL_TYPES::STT_FUNC,
             LIEF::ELF::SYMBOL_BINDINGS::STB_LOCAL,
-            // src_sym.other(),
+            // dep_sym.other(),
             0,
-            out_text_id,
-            src_sym.value() - src_text_sec.virtual_address() +
-                (out_text_sec.virtual_address() + dst_text_sec.size()),
-            src_sym.size());
-        out_->add_static_symbol(out_sym);
+            fat_text_id,
+            fat_text_sec.virtual_address() + text_off_ +
+                (dep_sym.value() - dep_text_sec.virtual_address()),
+            dep_sym.size());
+        args_.fat_->add_static_symbol(fat_sym);
     }
 }
 
